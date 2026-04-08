15761a1486bb10acf3e73b4198dd5b69b3c0674a
kevin Lis <kevin.lis@uniminuto.edu.co>
2026-04-08 18:46:52 -0500

    Inicio del proyecto del curso

---
+++ Creación_Apps_Web/storeapp/src/app/app.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import {} from './domains/products/pages/list/list'
+
+
+export const routes: Routes = [
+    {
+        path: '',
+        component: ListComponent
+    }
+];
